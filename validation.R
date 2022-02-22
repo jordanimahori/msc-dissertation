aeb3bbc1e8cb9886db0c7412f1a4b5914a8e68c6
Jordan Imahori <jordanimahori@gmail.com>
2022-02-22 21:30:56 +0000

    Add check for consistency across locations between CSV and GeoJSON

--- validation.R
+++ validation.R
@@ -4,15 +4,15 @@
 
 
 # --------------------------- ENVIRONMENT ---------------------------
+
 rm(list = ls())
 setwd("~/Projects/Dissertation/agro-welfare")
 
-library(dplyr)
-library(magrittr)
-
-
+library(tidyverse)
+library(sf)
 
 # ------------------------------ DATA -------------------------------
+
 # Read pre-cleaned data into memory
 lsla <- readRDS("./Data/lsla.R")
 areas_sp <- readRDS("./Data/areas_sp.R")
@@ -23,52 +23,57 @@ locations_sp <- readRDS("./Data/locations_sp.R")
 # --------------------- VALIDATE TABULAR DATA ------------------------
 
 # NOTES: The original LandMatrix files have a considerable (~335) number of 
-# duplicates in them.  
-
-# Checking for Deal IDs that appear in only the tabular or spatial files. 
-# IDs 3117 and 7648 appear only in the tabular data.
-# There are no IDs which appear only in the spatial data.
-
-sum(is.na(match(lsla$deal_id, locations_sp$deal_id))) # two IDs only appear in lsla
-sum(is.na(match(locations_sp$deal_id, lsla$deal_id))) # no IDs only appear in locations_sp
-
-missing_from_locations_sp <-lsla %>%      # stores observations for missing locations
-  filter(!(lsla$deal_id %in% locations_sp$deal_id)) 
-
-locations_sp %>%
-  filter(!(locations_sp$deal_id %in% lsla$deal_id))
+# duplicates in them. This falls to 101 (or 71 for the GeoJSON) once we drop 
+# observations wherethere is only imprecise coordinates (see: data_preparation.R)
 
 
-# Checking for duplicated IDs in tabular data (not checking spatial since subset).
-# There are 175 IDs which have duplicates, and 510 entries matching those IDs. 
-# Correspondingly, there are 335 "extra" observations. As a sanity check, 
-# 1965 - 335 = 1630, which is the number of (unique) deals we have in our deals data. 
-duplicated_ids <- unique(lsla$deal_id[duplicated(lsla$deal_id)])
-entries_for_duplicated_ids <- lsla %>% 
-  filter(lsla$deal_id %in% duplicated_ids)
+# Check for Deal IDs that appear in only the tabular or spatial files. 
+sum(is.na(match(lsla$deal_id, locations_sp$deal_id)))     # no IDs only in lsla
+sum(is.na(match(locations_sp$deal_id, lsla$deal_id)))     # no IDs only in locations_sp
+
+# Check for duplicate deal_id in lsla
+duplicated_ids <- unique(lsla$deal_id[duplicated(lsla$deal_id)]) # there are 38
+duplicated_ids_locations_sp <- unique(locations_sp$deal_id[duplicated(locations_sp$deal_id)]) # there are 34
+all(duplicated_ids_locations_sp %in% duplicated_ids) # GeoJSON a subset of CSV
+
+# Identify observations matching duplicated deal_id
+duplicated_obs_lsla <- lsla %>% 
+  filter(lsla$deal_id %in% duplicated_ids)            # there are 139 in CSV
+duplicated_obs_locations_sp <- locations_sp %>%
+  filter(locations_sp$deal_id %in% duplicated_ids)    # there are 109 in GeoJSON
+
+# Confirm number of unique obs by deal_id is the same across the two sources.
+length(unique(locations_sp$deal_id)) == length(unique(lsla$deal_id))   # is TRUE
+
+# Creating CSV of duplicated entries to send to LandMatrix
+write_csv(duplicated_obs, "duplicated_entries_CSV.csv")
+write_csv(duplicated_obs_locations_sp, "duplicated_entries_GeoJSON.csv")
 
 
-# Check whether observations with non-duplicated ID match those in the spatial dataset.
+
+# ---------------------- VALIDATE GEOJSON DATA ------------------------
+
+# Here, I check that the non-duplicated observations are identical in deal_id 
+# and location between the CSV and GeoJSON. They are, so we can be confident the 
+# underlying data is the same. 
+
+
+# Identify non-duplicated observations for CSV and GeoJSON sources
 non_dup_lsla <- lsla %>%
   filter(!(lsla$deal_id %in% duplicated_ids)) %>%
-  filter(!(deal_id == 3117 | deal_id == 7648)) %>% # some IDs not in other data
   arrange(deal_id)
 
 non_dup_locations_sp <- locations_sp %>%
   filter(!(locations_sp$deal_id %in% duplicated_ids)) %>%
   arrange(deal_id)
 
-non_dup_lsla$point %>%
-  sf()
+# Check that non-duplicated observations have the same deal_ids
+all(non_dup_lsla$deal_id %in% non_dup_locations_sp$deal_id)    # TRUE
+all(non_dup_locations_sp$deal_id %in% non_dup_lsla$deal_id)    # TRUE
 
+# Check locations of non-duplicated entries match
+st_distance(non_dup_lsla, non_dup_locations_sp, by_element=TRUE)  # result is zero vector
 
-
-# Creating CSV of duplicated entries to send to LandMatrix
-write_csv(entries_for_duplicated_ids, "duplicated_entries.csv")
-
-
-
-# ---------------------- VALIDATE GEOJSON DATA ------------------------
 
 
 
